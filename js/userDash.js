a564772cb1049042cbe75c6a47a1aa02572ad26c
Niall_Hogan <niall_hogan@hotmail.com>
2021-01-12 11:59:15 +0000

    linked pages

--- js/userDash.js
+++ js/userDash.js
@@ -3,3 +3,9 @@ document.querySelector("#logout").addEventListener("click", function(ev)
     console.log("WORKED");
     location.href = "../ElancoFrontend/index.html";
 })
+
+document.querySelector("#addBtn").addEventListener("click", function(ev)
+{
+    console.log("WORKED");
+    location.href = "../ElancoFrontend/addReceipts.html";
+})
